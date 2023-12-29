cf931e9c82ba813ffff8517da61fa7623d99f21a
hryesf <houriye.esfahanian@gmail.com>
2023-12-29 11:07:27 +0330

    Add @NaturalId for phoneNumber and delete unnecessary annotations

--- src/main/java/com/blubank/doctorappointment/entity/Patient.java
+++ src/main/java/com/blubank/doctorappointment/entity/Patient.java
@@ -3,9 +3,9 @@ package com.blubank.doctorappointment.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -25,11 +25,10 @@ public class Patient extends BaseEntity{
             columnDefinition = "varchar(50) default 'unknown'")
     private String fullName;
 
+    @NaturalId
     @NotNull(message = "Phone number is required!")
     @NotBlank(message = "Phone number can not be empty!")
-    @Pattern(regexp = "^09\\d{9}$", message = "invalid mobile number entered ")
-    @Digits(integer = 12, fraction = 0,
-            message = "The phone number is not correct!")
+    @Pattern(regexp = "^09\\d{9}$", message = "invalid phone number entered ")
     @Column(name = "phone_number",
             columnDefinition = "varchar(20)",
             unique = true,
