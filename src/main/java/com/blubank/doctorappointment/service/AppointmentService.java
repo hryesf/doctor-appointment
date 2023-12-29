84cc7e924530131129cdc0af090b54ad4b7889f8
hryesf <houriye.esfahanian@gmail.com>
2023-12-29 15:35:53 +0330

    Optimizing and cleaning the code

--- src/main/java/com/blubank/doctorappointment/service/AppointmentService.java
+++ src/main/java/com/blubank/doctorappointment/service/AppointmentService.java
@@ -1,22 +1,24 @@
 package com.blubank.doctorappointment.service;
 
 import com.blubank.doctorappointment.entity.Appointment;
+import com.blubank.doctorappointment.entity.AppointmentState;
 import com.blubank.doctorappointment.entity.Doctor;
 import com.blubank.doctorappointment.entity.Patient;
 import com.blubank.doctorappointment.exception.InvalidStartAndEndTimeException;
 import com.blubank.doctorappointment.exception.NotFoundException;
 import com.blubank.doctorappointment.exception.TakenAppointmentException;
 import com.blubank.doctorappointment.repository.AppointmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AppointmentService {
@@ -28,6 +30,7 @@ public class AppointmentService {
     private final PatientConverter patientConverter;
     private final AppointmentConverter appointmentConverter;
 
+    private final Logger logger = LoggerFactory.getLogger(AppointmentService.class);
 
     public AppointmentService(AppointmentRepository appointmentRepository, DoctorService doctorService, PatientService patientService, PatientConverter patientConverter, DoctorConverter doctorConverter, AppointmentConverter appointmentConverter) {
         this.appointmentRepository = appointmentRepository;
@@ -43,10 +46,9 @@ public class AppointmentService {
         return appointmentConverter.AppointmentDTOPaginated(appointmentRepository.findAll(pageRequest));
     }
 
-    public AppointmentDTO getAppointmentByDateTime(LocalDateTime dateTime) {
-        return appointmentConverter.toDto(appointmentRepository
-                .findAppointmentByAppointmentDateTime(dateTime)
-                .orElseThrow(() -> new NotFoundException("Appointment in selected time (" + dateTime + ") not found!")));
+    public AppointmentDTO getAppointmentById(Long id) {
+        return appointmentConverter.toDto(appointmentRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Appointment with id = " + id + " not found!")));
     }
 
     public Page<AppointmentDTO> getAppointmentsByPatientPhoneNumber(String patientPhoneNumber, int page, int size) {
@@ -55,45 +57,55 @@ public class AppointmentService {
                 appointmentRepository.findAppointmentsByPatientPhoneNumber(patientPhoneNumber, pageRequest));
     }
 
-    public Page<AppointmentDTO> getOpenAppointments(LocalDate date, int page, int size) {
+    public Page<AppointmentDTO> getOpenAppointments(LocalDateTime date, int page, int size) {
         PageRequest pageRequest = PageRequest.of(page, size);
         return appointmentConverter.AppointmentDTOPaginated(appointmentRepository.findOpenAppointments(date, pageRequest));
     }
 
-    public AppointmentDTO takeOpenAppointment(LocalDateTime dateTime, String phoneNumber) {
-        AppointmentDTO appointmentDTO = getAppointmentByDateTime(dateTime);
+    public AppointmentDTO takeOpenAppointment(Long appointmentId, String phoneNumber) {
+        AppointmentDTO appointmentDTO = getAppointmentById(appointmentId);
         Appointment appointment = appointmentConverter.toEntity(appointmentDTO);
 
-        if (appointment.getPatient() == null) {
-            PatientDTO patientDTO = patientService.getPatientByPhoneNumber(phoneNumber);
-            Patient patient = patientConverter.toEntity(patientDTO);
-
-            appointment.setPatient(patient);
-            appointment.setUpdatedAt(LocalDateTime.now());
-
-            return appointmentConverter.toDto(appointmentRepository.save(appointment));
-        } else {
+        if (appointment.getAppointmentState() == AppointmentState.TAKEN) {
             throw new TakenAppointmentException();
         }
+        PatientDTO patientDTO = patientService.getPatientByPhoneNumber(phoneNumber);
+        Patient patient = patientConverter.toEntity(patientDTO);
+
+        appointment.setPatient(patient);
+        appointment.setAppointmentState(AppointmentState.TAKEN);
+        appointment.setUpdatedAt(LocalDateTime.now());
+
+        try {
+            appointmentRepository.save(appointment);
+            return appointmentConverter.toDto(appointment);
+
+        } catch (OptimisticLockingFailureException e) {
+            logger.error("Optimistic locking failure for taking an appointment with id: {}", appointmentId);
+            throw e;
+        }
+
     }
 
-    public String deleteAppointmentById(Long id) {
-        Optional<Appointment> optionalAppointment = appointmentRepository.findById(id);
+    public String deleteAppointmentById(Long appointmentId) {
+        AppointmentDTO appointmentDTO = getAppointmentById(appointmentId);
+        Appointment appointment = appointmentConverter.toEntity(appointmentDTO);
 
-        if (optionalAppointment.isPresent()) {
-            Appointment appointment = optionalAppointment.get();
-            if (appointment.getPatient() != null) {
-                appointmentRepository.deleteById(id);
-                return "Appointment with code " + id + " removed from the list";
-            } else {
-                throw new TakenAppointmentException();
-            }
-        } else {
-            throw new NotFoundException("Appointment with id = " + id + " not found!");
+        if (appointment.getAppointmentState() == AppointmentState.TAKEN || appointment.getPatient() != null) {
+            throw new TakenAppointmentException();
+        }
+
+        try {
+            appointmentRepository.deleteById(appointmentId);
+            return "Appointment with code " + appointmentId + " removed from the list";
+
+        } catch (OptimisticLockingFailureException e) {
+            logger.error("Optimistic locking failure for taking an appointment with id: {}", appointmentId);
+            return "Appointment with id = " + appointmentId + " can not remove from the list! \nPlease try again later.";
         }
     }
 
-    //Doctor
+
     public String saveAppointments(Long doctorId, LocalDateTime startTime, LocalDateTime endTime) {
 
         if (endTime.isBefore(startTime)) {
