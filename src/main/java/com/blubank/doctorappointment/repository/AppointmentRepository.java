1636363e7c8bd95636fb4e1328794e933345e9bc
hryesf <houriye.esfahanian@gmail.com>
2023-12-29 16:04:07 +0330

    Add Optimistic Lock

--- src/main/java/com/blubank/doctorappointment/repository/AppointmentRepository.java
+++ src/main/java/com/blubank/doctorappointment/repository/AppointmentRepository.java
@@ -4,20 +4,27 @@ import com.blubank.doctorappointment.entity.Appointment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
-    @Query("SELECT a FROM Appointment a WHERE a.patient IS NULL AND DATE(a.appointmentDateTime) = :date")
-    Page<Appointment> findOpenAppointments(@Param("date") LocalDate date, PageRequest pageRequest);
-    Optional<Appointment> findAppointmentByAppointmentDateTime(LocalDateTime dateTime);
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "0")})
+    Optional<Appointment> findById(Long id);
+
+    @Query("SELECT a FROM Appointment a WHERE a.appointmentState = 'AVAILABLE' AND a.appointmentDateTime > :currentDateTime")
+    Page<Appointment> findOpenAppointments(@Param("date") LocalDateTime dateTime, PageRequest pageRequest);
+
     Page<Appointment> findAppointmentsByPatientPhoneNumber(String patientPhoneNumber, PageRequest pageRequest);
 
 
