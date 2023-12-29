6838503adf4a469a45e104f74cd1a1a8257e729a
hryesf <houriye.esfahanian@gmail.com>
2023-12-29 11:08:55 +0330

    modify Error message

--- src/main/java/com/blubank/doctorappointment/exception/TakenAppointmentException.java
+++ src/main/java/com/blubank/doctorappointment/exception/TakenAppointmentException.java
@@ -2,6 +2,6 @@ package com.blubank.doctorappointment.exception;
 
 public class TakenAppointmentException extends  RuntimeException{
     public TakenAppointmentException() {
-        super("Appointment is taken by a patient, and can not be removed!");
+        super("Appointment is taken by a patient!");
     }
 }
