60d470680a35ac1b80e086fcc6792e20bd3a8b5c
hryesf <houriye.esfahanian@gmail.com>
2023-12-29 15:27:45 +0330

    Add AppointmentState Enum

---
+++ src/main/java/com/blubank/doctorappointment/entity/AppointmentState.java
@@ -0,0 +1,6 @@
+package com.blubank.doctorappointment.entity;
+
+public enum AppointmentState {
+    AVAILABLE,  // The appointment is available for scheduling
+    TAKEN       // The appointment has been taken by a patient
+}
